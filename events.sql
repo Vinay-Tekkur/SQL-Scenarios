7c3096e1100a708ac400bde528854c8d39975b7e
Vinay Tekkur <38867261+Vinay-Tekkur@users.noreply.github.com>
2023-01-12 12:25:36 +0530

    Create events.sql

---
+++ events.sql
@@ -0,0 +1,21 @@
+CREATE TABLE random_tables.session_logs (ts DATETIME, session_id INT, user_id INT, log TEXT);
+
+INSERT INTO random_tables.session_logs  (ts, session_id, user_id, log) 
+VALUES 
+('2022-10-04 08:14:07', '898812', '523', 'CLICKED | Courses Buttom'),
+('2022-10-14 08:18:35', '898812', '523', 'NAVIAGE BACK | Python course page , codebasics.io'),
+('2022-10-16 12:07:00', '965345', '523', 'REVIEW GENERATED | Data analytics in power bi'),
+('2022-10-22 14:09:22', '188567', '707', 'NEW LOGIN | New login, user name: tasty@jalebi.com'),
+('2022-10-22 18:10:06', '188567', '707', 'COURSE PURCHASED | Data analytics in power bi, user name: tasty@jalebi.com');
+ 
+ delimiter |
+ CREATE EVENT e_daily_log_purge
+ on schedule every 10 second
+ comment "purge logs that are 5 days or older"
+ do
+ begin
+ DELETE FROM session_logs
+ WHERE DATE(ts) < DATE("2022-10-23") - interval 5 day;
+ end |
+ delimiter ;
+ 
